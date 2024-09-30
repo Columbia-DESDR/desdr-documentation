bf9e5404f0d69a988e71742e035c3ceb8230d537
Tanisha Bisht <agamyatani@gmail.com>
2024-09-30 08:23:57 -0400

    demo: update doc page

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Example Guide Copy', slug: 'guides/examplecopy' },
 					],
 				},
 				{
